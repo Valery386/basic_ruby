826990cf56db60020857a48f5939af2e0362d0cb
“Valery <valery220995@gmail.com>
2015-07-11 15:48:58 -0600

    Add first name and last name class

--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -1,2 +1,28 @@
+# class User
+#   def first_name=(value) # Es de instancia
+#     @first_name = value  #Set del nombre
+#   end
+
+
+#   def first_name
+#     @first_name #Get del nombre 
+#   end
+
+# end 
+
 class User
+    attr_accessor :last_name #Método de clase
+    attr_reader :age #hace el set
+    attr_writer :age #hace el get
+
+  def first_name=(value) # Método de clase si se coloca self
+    @first_name = value  #Set del nombre
+  end
+
+
+  def first_name
+    @first_name #Get del nombre 
+  end
+
 end 
+
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  
+  it "sets the first name" do
+    user = User.new
+    user.first_name = "Vale"
+
+    expect(user.first_name).to eq "Vale" 
+  end #test
+
+  it "sets the last name" do
+    user = User.new
+    user.last_name = "Méndez"
+
+    expect(user.last_name).to eq "Méndez"
+  end #test
+
+  it "sets the age" do
+    user = User.new
+    user.age = 19
+
+    expect(user.age).to eq 19
+  end #test
+
 end
